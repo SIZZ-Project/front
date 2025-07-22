2b99bab79e6e4c81980424526311a51037b7cf02
HandsomeBoy <87753974+GanziMan@users.noreply.github.com>
2025-07-22 18:11:06 +0900

    feat: 새로운 아이콘 및 뉴스 컴포넌트 추가, 페이지 레이아웃 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,23 +3,25 @@ import { Inter } from "next/font/google";
 import { ThemeProvider } from "next-themes";
 import "../styles/globals.css";
 import NavBar from "@/components/NavBar";
+import Footer from "@/components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-    title: "SIZZ 뉴스",
-    description: "공정하고 신뢰할 수 있는 뉴스 플랫폼",
+  title: "SIZZ 뉴스",
+  description: "공정하고 신뢰할 수 있는 뉴스 플랫폼",
 };
 
 export default function RootLayout({ children }: { children: ReactNode }) {
-    return (
-        <html lang="ko" suppressHydrationWarning>
-            <body className={inter.className}>
-                <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-                    <NavBar />
-                    {children}
-                </ThemeProvider>
-            </body>
-        </html>
-    );
+  return (
+    <html lang="ko" suppressHydrationWarning>
+      <body className={`${inter.className} min-h-screen flex flex-col`}>
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+          <NavBar />
+          <main className="flex-grow">{children}</main>
+          <Footer />
+        </ThemeProvider>
+      </body>
+    </html>
+  );
 }
