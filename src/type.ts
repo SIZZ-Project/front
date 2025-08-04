5191757f8eb51abe6cb83ad0375d85bb6213ba9a
HandsomeBoy <87753974+GanziMan@users.noreply.github.com>
2025-08-04 17:59:02 +0900

    feat: swagger 기반 타입 정의

---
+++ src/type.ts
@@ -0,0 +1,107 @@
+export interface CommentRequest {
+  articleId: number;
+  userId: string;
+  content: string;
+}
+
+export interface CommentResponse {
+  id: number;
+  articleId: number;
+  userId: string;
+  content: string;
+  createdAt: string; // ISO 8601 date string
+}
+
+export interface AuthRequest {
+  email: string;
+  password: string;
+  nickname: string;
+}
+
+export interface ApiResponse<T> {
+  success?: boolean;
+  message?: string;
+  data?: T;
+}
+
+export interface BookmarksRequest {
+  userId: number;
+  articleId: number;
+  bookmarked?: boolean;
+}
+
+export interface BookmarksResponse {
+  id: number;
+  userId: number;
+  articleId: number;
+  bookmarked?: boolean;
+}
+
+export interface Pageable {
+  page: number; // 최소 0
+  size: number; // 최소 1
+  sort?: string[];
+}
+
+export interface PageSearchNewsResponseDto {
+  totalElements: number;
+  totalPages: number;
+  pageable: PageableObject;
+  size: number;
+  content: SearchNewsResponseDto[];
+  number: number;
+  sort: SortObject;
+  numberOfElements: number;
+  first: boolean;
+  last: boolean;
+  empty: boolean;
+}
+
+export interface PageableObject {
+  paged: boolean;
+  pageNumber: number;
+  pageSize: number;
+  offset: number;
+  sort: SortObject;
+  unpaged: boolean;
+}
+
+export interface SearchNewsResponseDto {
+  articleId: string;
+  title: string;
+  description: string;
+  link: string;
+  pubDate: string; // ISO 8601 date string
+  sourceName: string;
+}
+
+export interface SortObject {
+  sorted: boolean;
+  empty: boolean;
+  unsorted: boolean;
+}
+
+export interface NewsResponseDto {
+  articleId: string;
+  title: string;
+  description: string;
+  link: string;
+  category: string[];
+  pubDate: string; // ISO 8601 date string
+  sourceName: string;
+  viewCount: number;
+}
+
+export interface PageNewsResponseDto {
+  totalElements: number;
+  totalPages: number;
+  pageable: PageableObject;
+  size: number;
+  content: NewsResponseDto[];
+  number: number;
+  sort: SortObject;
+  numberOfElements: number;
+  first: boolean;
+  last: boolean;
+  empty: boolean;
+}
