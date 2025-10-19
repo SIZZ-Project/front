e17d65401066c56b1c7c53be99f72b682a1955bf
HandsomeBoy <87753974+GanziMan@users.noreply.github.com>
2025-10-19 23:50:57 +0900

    feat: Add authentication check and display personalized news recommendations in News component

--- src/app/(home)/_components/home-bottom/News.tsx
+++ src/app/(home)/_components/home-bottom/News.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import NewsCard from "./NewsCard";
 import { twMerge } from "tailwind-merge";
 import { NewsResponseDto, PageNewsResponseDto } from "@/type";
+import { isTokenValid } from "@/libs/auth";
 
 interface NewsProps {
   news: PageNewsResponseDto;
@@ -11,7 +12,7 @@ interface NewsProps {
 export default function News({ news, hotNews }: NewsProps) {
   const isScroll = news.content.length >= 3;
   const isScrollHot = hotNews.length >= 3;
-
+  const isAuthenticated = isTokenValid();
   return (
     <div className="flex flex-col gap-[200px] mx-auto bg-[rgba(18,22,25,0.25)] p-16">
       <div>
@@ -49,9 +50,10 @@ export default function News({ news, hotNews }: NewsProps) {
           ))}
         </div>
       </div>
+
       <div>
         <div className="text-[40px] font-bold leading-[150%] text-coolGray-10">
-          관심 뉴스 추천
+          선택 뉴스 추천
         </div>
 
         <div className="bg-[#343A3F] h-[3px] w-full" />
@@ -87,6 +89,47 @@ export default function News({ news, hotNews }: NewsProps) {
           )}
         </div>
       </div>
+
+      {isAuthenticated && (
+        <div>
+          <div className="text-[40px] font-bold leading-[150%] text-coolGray-10">
+            관심 뉴스 추천
+          </div>
+
+          <div className="bg-[#343A3F] h-[3px] w-full" />
+
+          <div className="flex gap-6 justify-between mt-1">
+            {hotNews.length === 0 ? (
+              <div
+                style={{
+                  background:
+                    "linear-gradient(104deg, rgba(255, 255, 255, 0.12) 0.37%, rgba(255, 255, 255, 0.20) 99.63%), rgba(0, 0, 0, 0.80)",
+                }}
+                className="w-full h-[470px]  flex items-center justify-center rounded-xl"
+              >
+                <p className="text-coolGray-30 text-2xl leading-[150%]">
+                  관심 뉴스 추천이 없습니다.
+                </p>
+              </div>
+            ) : (
+              hotNews.map((item) => (
+                <NewsCard
+                  key={item.articleId}
+                  category={item.category[0] as "정치" | "진보" | "중립"}
+                  className={twMerge(
+                    "aspect-[748/470] w-full max-h-[470px]",
+                    isScrollHot ? "md:w-[49%] flex-shrink-0" : "w-[50%]"
+                  )}
+                  title={item.title}
+                  content={item.description}
+                  date={item.pubDate.split("T")[0]}
+                  newsData={item}
+                />
+              ))
+            )}
+          </div>
+        </div>
+      )}
     </div>
   );
 }
